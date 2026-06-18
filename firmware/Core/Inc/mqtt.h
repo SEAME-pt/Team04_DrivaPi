478eeca145df1aa6458deb5c7069ffa2aa7ea1db
Hugo Lopes <hugofslopes@live.com.pt>
2026-06-18 15:51:10 +0100

    add tls to mqtt

--- firmware/Core/Inc/mqtt.h
+++ firmware/Core/Inc/mqtt.h
@@ -12,11 +12,15 @@ extern "C" {
 int stm32_mqtt_send(Network* n, unsigned char* buffer, int len, int timeout_ms);
 int stm32_mqtt_recv(Network* n, unsigned char* buffer, int len, int timeout_ms);
 void mqtt_thread_fc(ULONG thread_input);
+void stm32_emergency_callback(MessageData* data);
 
 #ifndef MX_WIFI_DEBUG
-#define MX_WIFI_DEBUG                  (1)
+#define MX_WIFI_DEBUG	(1)
 #endif
 
+#ifndef EMERGENCY_VEHICLE
+#define EMERGENCY_VEHICLE	 (1)
+#endif
 
 #ifdef __cplusplus
 }
